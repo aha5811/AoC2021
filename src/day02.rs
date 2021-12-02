4ce7c3cc42e93567991730b04c9dca82510d3966
inf6009 <hoernlein@uni-wuerzburg.de>
2021-12-02 11:36:39 +0100

    day2

---
+++ src/day02.rs
@@ -0,0 +1,52 @@
+pub fn day02_1(s: &str) -> i32 {
+    let cmnds = to_cmds(super::util::read_lines(s));
+    let mut h = 0;
+    let mut v = 0;
+    for cmd in cmnds {
+        match cmd.d {
+            Dir::F => { h = h + cmd.n },
+            Dir::B => { h = h - cmd.n },
+            Dir::U => { v = v - cmd.n },
+            Dir::D => { v = v + cmd.n },
+            _ => {} // won't happen
+        }
+    }
+    h * v
+}
+
+enum Dir { F, B, U, D, ERR }
+struct Cmd { d: Dir, n: i32 }
+
+fn to_cmds(v: Vec<String>) -> Vec<Cmd> {
+    let mut ret = Vec::new();
+    for s in v {
+        let cmd: Vec<&str> = s.split(' ').collect();
+        let d: Dir = match cmd[0] {
+            "forward" => Dir::F,
+            "backward" => Dir::B,
+            "up" => Dir::U,
+            "down" => Dir::D,
+            _ => Dir::ERR
+        };
+        let n: i32 = cmd[1].parse().unwrap();
+        ret.push(Cmd { d: d, n: n });
+    }
+    ret
+}
+
+pub fn day02_2(s: &str) -> i32 {
+    let cmnds = to_cmds(super::util::read_lines(s));
+    let mut h = 0;
+    let mut v = 0;
+    let mut dspeed = 0; // aim
+    for cmd in cmnds {
+        match cmd.d {
+            Dir::F => { h = h + cmd.n; v = v + cmd.n * dspeed; },
+            Dir::B => { h = h - cmd.n; v = v + cmd.n * dspeed; },
+            Dir::U => { dspeed = dspeed - cmd.n },
+            Dir::D => { dspeed = dspeed + cmd.n },
+            _ => {} // won't happen
+        }
+    }
+    h * v
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 mod util;
 mod day01;
+mod day02;
 
 fn main() {
     println!("day01_1: {}", day01::day01_1("day01_1_input"));
     println!("day01_2 test: {}", day01::day01_2("day01_2_test"));
     println!("day01_2: {}", day01::day01_2("day01_1_input"));
+    println!("-------------------------------------");
+    println!("day02_1: {}", day02::day02_1("day02_1_input"));
+    println!("day01_2 test 900: {}", day02::day02_2("day02_2_test"));
+    println!("day02_2: {}", day02::day02_2("day02_1_input"));
+    println!("-------------------------------------");
 }
