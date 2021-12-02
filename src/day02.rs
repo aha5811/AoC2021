a7c843a30650dc0e3ae5b847079398c3c21367be
inf6009 <hoernlein@uni-wuerzburg.de>
2021-12-02 13:37:36 +0100

    adds FromStr to Dir enum

--- src/day02.rs
+++ src/day02.rs
@@ -3,33 +3,42 @@ pub fn day02_1(filename: &str) -> i32 {
     let mut h = 0;
     let mut v = 0;
     for cmd in cmnds {
-        match cmd.d {
+        match cmd.dir {
             Dir::F => { h = h + cmd.n },
             Dir::B => { h = h - cmd.n },
             Dir::U => { v = v - cmd.n },
-            Dir::D => { v = v + cmd.n },
-            _ => {} // won't happen
+            Dir::D => { v = v + cmd.n }
         }
     }
     h * v
 }
 
-enum Dir { F, B, U, D, ERR }
-struct Cmd { d: Dir, n: i32 }
+enum Dir { F, B, U, D }
+
+impl std::str::FromStr for Dir {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "forward" => Ok(Dir::F),
+            "backward" => Ok(Dir::B),
+            "up" => Ok(Dir::U),
+            "down" => Ok(Dir::D),
+            _ => Err(format!("'{}' invalid Dir", s))
+        }
+    }
+}
+
+struct Cmd { dir: Dir, n: i32 }
 
 fn to_cmds(strings: Vec<String>) -> Vec<Cmd> {
     let mut ret = Vec::new();
     for s in strings {
         let cmd: Vec<&str> = s.split(' ').collect();
-        let d: Dir = match cmd[0] {
-            "forward" => Dir::F,
-            "backward" => Dir::B,
-            "up" => Dir::U,
-            "down" => Dir::D,
-            _ => Dir::ERR
-        };
-        let n: i32 = cmd[1].parse().unwrap();
-        ret.push(Cmd { d: d, n: n });
+        ret.push(Cmd {
+            dir: cmd[0].parse().unwrap(),
+            n: cmd[1].parse().unwrap()
+        });
     }
     ret
 }
@@ -40,12 +49,11 @@ pub fn day02_2(filename: &str) -> i32 {
     let mut v = 0;
     let mut dspeed = 0; // aim
     for cmd in cmnds {
-        match cmd.d {
+        match cmd.dir {
             Dir::F => { h = h + cmd.n; v = v + cmd.n * dspeed; },
             Dir::B => { h = h - cmd.n; v = v + cmd.n * dspeed; },
             Dir::U => { dspeed = dspeed - cmd.n },
-            Dir::D => { dspeed = dspeed + cmd.n },
-            _ => {} // won't happen
+            Dir::D => { dspeed = dspeed + cmd.n }
         }
     }
     h * v
