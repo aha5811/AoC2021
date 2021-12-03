b69ef3926647987f5680a491f02c465eec5931b7
inf6009 <hoernlein@uni-wuerzburg.de>
2021-12-03 14:13:42 +0100

    * simplifies filter for part2 * simplifies string building in part1

--- src/day03.rs
+++ src/day03.rs
@@ -1,6 +1,7 @@
 pub fn part1(filename: &str) -> i32 {
     let strings = crate::util::read_lines(filename);
     
+    // length of strings
     let len = strings.iter().next().unwrap().len();
 
     let mut cnt1s = vec!(0; len);
@@ -12,22 +13,16 @@ pub fn part1(filename: &str) -> i32 {
         }
     }
 
-    let thh = strings.len() / 2;
-
     let mut gamma = "".to_owned();
     let mut epsilon = "".to_owned();
-    for i in 0..len {
-        let gd;
-        let ed;
-        if cnt1s[i] >= thh {
-            gd = "1";
-            ed = "0";
-        } else {
-            gd = "0";
-            ed = "1";
+    {
+        let thh = strings.len() / 2;
+
+        for i in 0..len {
+            let more1 = cnt1s[i] >= thh;
+            gamma.push_str(if more1 { "1" } else { "0" });
+            epsilon.push_str(if more1 { "0" } else { "1" });
         }
-        gamma.push_str(gd);
-        epsilon.push_str(ed);
     }
 
     to_i32(&gamma) * to_i32(&epsilon)
@@ -37,47 +32,35 @@ fn to_i32(s: &str) -> i32 {
     isize::from_str_radix(s, 2).unwrap() as i32
 }
 
-fn filter(strings: Vec<String>, p: i32, most: bool) -> Vec<String> {
-    let size = strings.len();
+fn filter(strings: Vec<String>, pos: i32, most: bool) -> Vec<String> {
 
-    if size == 1 {
-        return strings;
-    }
+    let mut ret1 = Vec::new();
+    let mut ret0 = Vec::new();
 
-    let thh = size as f32 / 2.;
-
-    let mut cnt1 = 0;
-    for s in &strings {
-        let cc = s.chars().nth(p as usize).unwrap();
-        if cc == '1' {
-            cnt1 += 1
-        }
-    }
-
-    let filter;
-    if cnt1 as f32 >= thh {
-        filter = if most { '1' } else { '0' }
-    } else {
-        filter = if most { '0' } else { '1' }
-    }
-
-    let mut ret = Vec::new();
-    
     for s in strings {
-        let cc = s.chars().nth(p as usize).unwrap();
-        if cc == filter {
-            ret.push(s)
+        let cc = s.chars().nth(pos as usize).unwrap();
+        if cc == '1' {
+            ret1.push(s)
+        } else {
+            ret0.push(s)
         }
     }
 
-    ret
+    if (ret1.len() >= ret0.len()) ^ most {
+        ret0
+    } else {
+        ret1
+    }
 }
 
 fn get_filtered(strings: &Vec<String>, len: i32, most: bool) -> i32 {
     let mut filtered = strings.clone();
 
-    for p in 0.. len {
-        filtered = filter(filtered, p, most);
+    for pos in 0.. len {
+        filtered = filter(filtered, pos, most);
+        if filtered.len() == 1 {
+            break;
+        }
     }
 
     to_i32(filtered.iter().next().unwrap())
